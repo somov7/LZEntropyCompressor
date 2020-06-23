0502f1d287c9d5fee5c994973dc5ea2dacdc1619
Artyom Somov <avsomov7@gmail.com>
2020-06-23 16:14:39 +0300

    Some text

--- LZ77.hpp
+++ LZ77.hpp
@@ -2,8 +2,8 @@
 
 #include <cstdint>
 
-#define MAX_LENGTH 19
 #define MIN_LENGTH 4
+#define MAX_LENGTH 19
 #define MAX_JUMP 4096
 
 size_t lz77EncodeFast(uint8_t* inBuffer, size_t inSize, uint8_t *outBuffer, size_t outSize);
