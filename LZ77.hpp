cbf195f016561506b106a4a56c9ae15e0e96dc64
Artyom Somov <avsomov7@gmail.com>
2020-06-28 01:45:53 +0300

    Hotfix

--- LZ77.hpp
+++ LZ77.hpp
@@ -4,7 +4,7 @@
 
 #define MIN_LENGTH 4
 #define MAX_LENGTH 19
-#define MAX_JUMP 4096
+#define MAX_JUMP 4095
 
 size_t lz77EncodeFast(uint8_t* inBuffer, size_t inSize, uint8_t *outBuffer, size_t outSize);
 size_t lz77EncodeDeep(uint8_t* inBuffer, size_t inSize, uint8_t *outBuffer, size_t outSize);
